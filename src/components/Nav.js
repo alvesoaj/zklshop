cb94186b8c3afe0c12d58e6dd51a0aa5a01eb752
AJ Alves <aj.alves@zerokol.com>
2018-07-03 16:08:27 -0300

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+// functional component
+const NavItem = props => {
+  const pageURI = window.location.pathname+window.location.search
+  const liClassName = (props.path === pageURI) ? "nav-item active" : "nav-item";
+  const aClassName = props.disabled ? "nav-link disabled" : "nav-link"
+  return (
+    <li className={liClassName}>
+      <a href={props.path} className={aClassName}>
+        {props.name}
+        {(props.path === pageURI) ? (<span className="sr-only">(current)</span>) : ''}
+      </a>
+    </li>
+  );
+}
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="/">Start Bootstrap</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <NavItem path="/" name="Home" />
+              <NavItem path="/about" name="About" />
+              <NavItem path="/service" name="Services" />
+              <NavItem path="/contact" name="Contact" />
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
